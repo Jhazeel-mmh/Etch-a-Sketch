6e69e54eaa80509bd74f6f51690b47af03ff4ddb
Jhazeel Hernandez <jhazeelhdz@gmail.com>
2024-06-26 11:28:49 -0600

    Update the event listener of button and remove the previous grid before create the new

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let container = document.querySelector(".container");
 
-function createSquares (n = 16){
-    for (let i = 0; i < 16; i++){
+function createSquares (amount = 16){
+    for (let i = 0; i < amount; i++){
         let row = document.createElement("div");
         row.setAttribute("id", `row-${i}`);
         row.classList.toggle("row");
-        for (let n = 0; n < 16; n++){
+        for (let n = 0; n < amount; n++){
             let square = document.createElement("div");
             square.classList.toggle("square");
             square.setAttribute("id", `${i}-${n}`);
@@ -20,6 +20,21 @@ function changeSquareBackground(e){
     targetSquare.style.backgroundColor = "#e7324d";
 }
 
-document.addEventListener("DOMContentLoaded", createSquares);
+
+document.addEventListener("DOMContentLoaded", () => {
+   createSquares()
+});
+
 container.addEventListener("mouseover", changeSquareBackground);
 
+let btn = document.querySelector(".btn");
+btn.addEventListener("click", () => {
+    let choose;
+    while (true) {
+        choose = prompt("Type a number between 1 and 100 | Ingrese un número entre 1 y 100:");
+        choose = Number(choose);
+        if (choose > 0 && choose <= 100) break;
+    }
+    container.textContent = "";
+    createSquares(choose);
+});
