8f4dce9da8a21e887bd0c8012b0034f380a0f297
Jhazeel Hernandez <jhazeelhdz@gmail.com>
2024-06-25 22:55:03 -0600

    Fix the changeSquareBackground() the function now change the background-color of the squares

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ for (let i = 0; i < 16; i++){
         let container = document.querySelector(".container");
         let square = document.createElement("div");
         square.classList.toggle("square");
+        square.setAttribute("id", `${i}-${n}`)
         container.appendChild(square);
     }
 }
+
+function changeSquareBackground(e){
+    let targetSquare = e.target;
+    targetSquare.style.backgroundColor = "#e7324d";
+}
+
+
+let container = document.querySelector(".container");
+container.addEventListener("mouseover", changeSquareBackground);
+
